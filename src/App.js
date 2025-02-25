1ca231c1f6482c5acf8b5c549a1d82fbb98b47ae
omerbnk <73026541+omerbnk@users.noreply.github.com>
2025-02-25 22:52:40 +0300

    new version
    yazılara tıklama ozellgi geldi, bigliler sola yaslandı

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import './App.css';
 import logo from './logo1.png';
 
 
 function App() {
+  const [isHoveredForNumber, setIsHoveredForNumber] = useState(false);
+  const [isHoveredForEmail, setIsHoveredForEmail] = useState(false);
+  const [isHoveredForMap, setIsHoveredForMap] = useState(false);
+
+
+  const address = 'Cevizli Mahallesi, Bağdat Caddesi No: 599-601A Maltepe/İstanbul';  // Buraya adresi girin
+  const phoneNumber = "+90542 316 4774"; // Burada telefon numarasını yazın
+  const email = "hakancizmeci@hotmail.com";
+
+  const openMap = () => {
+    // Adresi URL'ye encode ederek Google Maps'te açma
+    const encodedAddress = encodeURIComponent(address);
+    const mapUrl = `https://www.google.com/maps?q=${encodedAddress}`;
+    window.open(mapUrl, '_blank'); // Yeni pencerede açılır
+  };
+
+
+  const handleCall = () => {
+    const telUrl = `tel:${phoneNumber}`; // Tel protokolünü kullanarak arama başlatılır
+    window.location.href = telUrl; // Telefon arama ekranını açar
+  };
+
+
+  const handleMail = () => {
+    const mailtoUrl = `mailto:${email}`; // Mailto protokolü ile mail adresini açma
+    window.location.href = mailtoUrl; // Varsayılan e-posta istemcisini açar
+  };
 
   return (
-    <div className="app-container" name="viewport" content= "with=device-with , initial-scale = 1.0" >
+    <div className="app-container" name="viewport" content="with=device-with , initial-scale = 1.0" >
       {/* Logo Animasyonu */}
       <motion.div
         className="logo"
@@ -30,12 +57,31 @@ function App() {
         animate={{ opacity: 1, y: 0 }}  // Tam görünür ve yukarı
         transition={{ delay: 1.5, duration: 1.5 }}  // Gecikmeli ve uzun geçiş süresi
       >
-   
-        <p>📍 <strong>Adres:</strong> Cevizli Mahallesi, Bağdat Caddesi No: 599-601A Maltepe/İstanbul</p>
-        <p>📞 <strong>Telefon:</strong> (542) 316 4774</p>
-        <p>📧 <strong>E-mail:</strong> hakancizmeci@hotmail.com</p>
-        <p>👨‍🍳 <strong>Hakan ÇİZMECİOĞLU</strong></p>
-     
+        <p align="left" onClick={openMap} onMouseEnter={() => setIsHoveredForMap(true)} // Hover başladığında
+          onMouseLeave={() => setIsHoveredForMap(false)} // Hover bittiğinde
+          style={{
+            color: isHoveredForMap ? '#e60000' : 'white', // Hover durumunda renk değişimi
+            cursor: 'pointer', // Tıklanabilir
+            transition: 'color 0.3s ease' // Geçiş efekti
+          }}>📍 <strong>Adres:</strong> Cevizli Mahallesi, Bağdat Caddesi No: 599-601A Maltepe/İstanbul</p>
+        <p align="left" onClick={handleCall} 
+        onMouseEnter={() => setIsHoveredForNumber(true)} 
+        onMouseLeave={() => setIsHoveredForNumber(false)} // Hover bittiğinde
+        style={{
+          color: isHoveredForNumber ? '#e60000' : 'white', // Hover durumunda renk değişimi
+          cursor: 'pointer', // Tıklanabilir
+          transition: 'color 0.3s ease' // Geçiş efekti
+        }}>📞 <strong>Telefon:</strong> (542) 316 4774</p>
+        <p align="left" onClick={handleMail} 
+        onMouseLeave={() => setIsHoveredForEmail(false)} // Hover bittiğinde
+        onMouseEnter={() => setIsHoveredForEmail(true)} 
+        style={{
+          color: isHoveredForEmail ? '#e60000' : 'white', // Hover durumunda renk değişimi
+          cursor: 'pointer', // Tıklanabilir
+          transition: 'color 0.3s ease' // Geçiş efekti
+        }}>📧 <strong>E-mail:</strong> hakancizmeci@hotmail.com</p>
+        <p align="left">👨‍🍳 <strong>Hakan ÇİZMECİOĞLU</strong></p>
+
       </motion.div>
     </div>
   );
